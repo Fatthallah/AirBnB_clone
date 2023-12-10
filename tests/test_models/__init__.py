3182467db72d994b35c8a9b42e71223e968412a2
KhaledHemdan <136645522+KhaledHemdan@users.noreply.github.com>
2023-12-10 23:51:14 +0200

    Delete tests directory

--- tests/test_models/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-'''This is the comment I Have to write'''
-from models.engine.file_storage import FileStorage
-
-
-
-'''This is the comment I Have to write'''
-storage = FileStorage()
-storage.reload()
