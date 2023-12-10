2e91bf0e8e3e85065996ca54b57a19f4a8681a40
Fatthallah <eng.fatthallah@gmail.com>
2023-12-10 03:16:42 -0800

    X

---
+++ models/engine/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''This is the comment I Have to write'''
+'''This is the comment I Have to write'''
+'''This is the comment I Have to write'''
+from models.engine.file_storage import FileStorage
+
+'''This is the comment I Have to write'''
+'''This is the comment I Have to write'''
+'''This is the comment I Have to write'''
+storage = FileStorage()
+storage.reload()
+
