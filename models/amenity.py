826560e7b9f0f135a90f45c3d3cc80518f6fc801
Fatthallah <eng.fatthallah@gmail.com>
2023-12-10 03:09:23 -0800

    x

---
+++ models/amenity.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''This is the comment I Have to write'''
+'''This is the comment I Have to write'''
+'''This is the comment I Have to write'''
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    '''This is the comment I Have to write'''
+    '''This is the comment I Have to write'''
+    '''This is the comment I Have to write'''
+    name = ""
+    def __init__(self, *args, **kwargs):
+        """This is the comment I Have to write
+
+    This is the comment I Have to write
+
+    This is the comment I Have to write
+    This is the comment I Have to write
+    """
+        super().__init__(*args, **kwargs)
+
---
+++ models/city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''This is the comment I Have to write'''
+'''This is the comment I Have to write'''
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    '''This is the comment I Have to write'''
+    state_id = ""
+    name = ""
+    def __init__(self, *args, **kwargs):
+    """This is the comment I Have to write
+
+    This is the comment I Have to write
+
+    This is the comment I Have to write
+    This is the comment I Have to write
+    """
+        super().__init__(*args, **kwargs)
+
